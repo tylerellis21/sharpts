ee33e8ca2eb9559f005c1f5084b3e10394889fa1
Tyler Ellis <tyler@locksis.org>
2020-09-22 01:12:27 -0700

    Rewrote the regex test method as a lambda

--- SharpTS/Reflection/TypeRegex.cs
+++ SharpTS/Reflection/TypeRegex.cs
@@ -5,16 +5,9 @@ namespace SharpTS.Reflection {
     
     public static class TypeRegex { 
 
-        public static bool RegexTestType(Type type, string pattern) {
-            
-            string type_name = type.FullName;
-
-            // Find matches.
-            MatchCollection matches = Regex.Matches(type_name, pattern);
-
-            return matches.Count > 0;
-        } // void RegexTestType 
-
+        public static bool RegexTestType(Type type, string pattern)
+            => Regex.Matches(type.FullName, pattern).Count > 0;
+        
     } // class TypeRegex
 
 } // namespace SharpTS.Reflection
