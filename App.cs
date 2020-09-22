f89b0cb47cb8e394ae782712cafb8fd10b9807b5
Tyler Ellis <tyler@locksis.org>
2020-09-21 20:08:47 -0700

    Added a class to hold the type reflection code

--- App.cs
+++ App.cs
@@ -9,18 +9,68 @@ namespace sharpts {
     internal class Program {
 
         static void Main(string[] args) => (new Program()).Run(args); 
-
-        void Run(string[] args) {
-            string input_namespace = "WP.Models";
-
-            string[] input_files;
-            string output_dir;
             
-            Assembly assembly = Assembly.GetCallingAssembly();
+        // The list of .net assembly files we will be searching for type definitions in.
+        static List<string> input_files = new List<string>();
 
-            List<Type> types = SearchNamespaceType(assembly, input_namespace);
+        // The list of namespaces we are processing.
+        static List<string> input_namespaces = new List<string>();
+
+        // By default we will just dump to the current working directory.
+        static string output_dir = "./";
+
+        
+        void ParseArguments(string[] args) {
+            // We could probably do this a better way but i just quickly wrote this argument parsing.
+            for (int i = 0; i < args.Length; i++) {
+                switch (args[i]) {
+                    default: 
+                        Console.WriteLine($"unknown argument: '{args[i]}'"); 
+                    break;
+                    
+                    case "-n":
+                    case "-namespace": {
+                        if (i + 1 > args.Length) {
+                            Console.WriteLine("no namespace specified!");
+                            return;
+                        }
+                        input_namespaces.Add(args[i + 1]);
+                    } break;
+
+                    case "-in":
+                    case "-i":  {
+                        if (i + 1 > args.Length) {
+                            Console.WriteLine("no input assembly specified!");
+                            return;
+                        }
+                        input_files.Add(args[i + 1]);
+                    } break;
+
+                    case "-out":
+                    case "-o": {
+                        if (i + 1 > args.Length) {
+                            Console.WriteLine("no namespace specified!");
+                            return;
+                        }                                         
+                        output_dir = args[i + 1];
+                    } break;
+
+                } // switch
+            } // for
+        } // ParseArguments
+
+        // Begin the process of generating typescript.
+        // 1. We must search for the types we need to processes.
+        // 2. Generate the appopriate typescript files for each type we are processing.
+        void Run(string[] args) {
+            
+            ParseArguments(args);
+
+            //Assembly assembly = Assembly.GetCallingAssembly();
+
+            //List<Type> types = SearchNamespaceType(assembly, input_namespace);
         }
-
+/*
         static List<Type> SearchNamespaceType(Assembly assembly, string name) {
             List<Type> types = new List<Type>();
 
@@ -32,6 +82,6 @@ namespace sharpts {
 
             return types;
         }
-
+*/
     } // class Program
 } // namespace sharpts
---
+++ SharpTS/TypeReflection.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace sharpts {
+
+    public static class TypeReflection {
+    
+    } // class TypeReflection
+    
+} // namespace sharpts
+ 
